81845df75404428b1f980701959bf5198d428389
Muthukumar0709 <138592091+Muthukumar0709@users.noreply.github.com>
2023-07-04 18:27:41 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,135 @@
+function createUser() {
+  var usernameInput = document.getElementById("userNm");
+  var enteredUsername = usernameInput.value.trim();
+
+  // Retrieve the existing users from local storage
+  var existingUsers = localStorage.getItem("users");
+
+  // Check if the users data exists
+  if (existingUsers) {
+    // Attempt to parse the data as JSON
+    try {
+      existingUsers = JSON.parse(existingUsers);
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      // Handle the error, such as setting existingUsers to an empty array
+      existingUsers = [];
+    }
+  } else {
+    // No user data found in localStorage, initialize as an empty array
+    existingUsers = [];
+  }
+  // Check if the username already exists
+  var existingUser = existingUsers.find(function (user) {
+    return user.username === enteredUsername;
+  });
+
+  if (existingUser) {
+    alert("Username already exists. Please choose a different username.");
+    usernameInput.value = ""; // Clear the input field
+    usernameInput.focus(); // Set focus back to the username input field
+  } else {
+    // Create a new user object
+    var newUser = {
+      username: enteredUsername,
+      isAdmin: false,
+    };
+
+    // Add the new user to the list
+    existingUsers.push(newUser);
+
+    // Update the users in local storage
+    localStorage.setItem("users", JSON.stringify(existingUsers));
+
+    alert("Account created successfully!");
+  }
+}
+
+function check() {
+  var usernameInput = document.getElementById("name");
+  var enteredUsername = usernameInput.value.trim();
+  var password = document.getElementById("userPw");
+
+  // Retrieve the existing users from local storage
+  var existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+  // Check if the username exists
+  var loggedInUser = existingUsers.find(function (user) {
+    return user.username === enteredUsername;
+  });
+
+  if (enteredUsername == "admin" && password.value == "admin") {
+    window.location.href = "dashboard.html";
+
+    var localStorageContent = JSON.stringify(existingUsers);
+    alert(localStorageContent);
+
+    // // Create a new <pre> element to display the localStorage content
+    // var contentElement = document.createElement("pre");
+    // contentElement.textContent = localStorageContent;
+
+    // // Append the <pre> element to a container in your HTML (e.g., <div id="contentContainer">)
+    // var container = document.getElementById("contentContainer");
+    // container.appendChild(contentElement);
+
+    alert("admin login successful!");
+  } else if (loggedInUser) {
+    window.location.href = "dashboard.html";
+    alert("User login successful!");
+    //     window.location.href = "/";
+  } else {
+    alert("Invalid username or password.");
+  }
+}
+
+// function check() {
+//   var usernameInput = document.getElementById("name");
+//   var enteredUsername = usernameInput.value.trim();
+//   var password = document.getElementById("userPw");
+
+//   // Retrieve the existing users from local storage
+//   var existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+//   // Check if the username exists
+//   var loggedInUser = existingUsers.find(function (user) {
+//     return user.username === enteredUsername;
+//   });
+
+//   if (enteredUsername === "admin" && password.value === "admin") {
+//     var localStorageContent = JSON.stringify(existingUsers);
+
+//     const container = document.getElementById("contentContainer");
+
+//     // Create an unordered list element
+//     const list = document.createElement("ul");
+
+//     // Iterate over the JSON properties and create list items
+//     for (const key in localStorageContent) {
+//       if (localStorageContent.hasOwnProperty(key)) {
+//         const listItem = document.createElement("li");
+//         listItem.innerHTML = `<strong>${key}:</strong> ${localStorageContent[key]}`;
+//         list.appendChild(listItem);
+//       }
+//     }
+//     alert(localStorageContent);
+//     // Append the list to the container element
+//     container.appendChild(list);
+
+//     // // Create a new <pre> element to display the localStorage content
+//     // var contentElement = document.createElement("pre");
+//     // contentElement.textContent = localStorageContent;
+
+//     // // Append the <pre> element to a container in your HTML (e.g., <div id="contentContainer">)
+//     // var container = document.getElementById("contentContainer");
+//     // container.innerHTML = ""; // Clear existing content
+//     // container.appendChild(contentElement);
+
+//     alert("admin login successful!");
+//     window.location.href = "dashboard.html";
+//   } else if (loggedInUser) {
+//     window.location.href = "dashboard.html";
+//     alert("User login successful!");
+//   } else {
+//     alert("Invalid username or password.");
+//   }
+// }
---
+++ user.js
@@ -0,0 +1,43 @@
+function createUser() {
+  var names = document.getElementById("userNm");
+  var pws = document.getElementById("pw");
+
+  if (names.value.length == 0) {
+    alert("Please fill in email");
+  } else if (pws.value.length == 0) {
+    alert("Please fill in password");
+  } else {
+    localStorage.setItem("names", names.value);
+    localStorage.setItem("pws", pws.value);
+    alert("Your account has been created");
+  }
+}
+
+//checking
+function check() {
+  var storedName = localStorage.getItem("names");
+  var storedPw = localStorage.getItem("pws");
+
+  var userName = document.getElementById("name");
+  var userPw = document.getElementById("userPw");
+  var userRemember = document.getElementById("rememberMe");
+
+  if (userName.value == "admin" && userPw.value == "admin") {
+    window.location.href = "dashboard.html";
+    alert("admin login successful!");
+    var store = localStorage.getItem("names");
+    alert(store);
+    var contentElement = document.createElement("pre");
+    contentElement.textContent = store;
+
+    // Append the <pre> element to a container in your HTML (e.g., <div id="contentContainer">)
+    var container = document.getElementById("contentContainer");
+    container.appendChild(contentElement);
+  } else if (userName.value == storedName && userPw.value == storedPw) {
+    var store = localStorage.getItem("name");
+    alert(store);
+    alert("You are logged in.");
+  } else {
+    alert("Error on login");
+  }
+}
